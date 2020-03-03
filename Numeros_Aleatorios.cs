e1e42a6df20610449dbfc084e83668c0b993daf5
esafla <61722079+esafla@users.noreply.github.com>
2020-03-03 12:34:53 -0500

    Add files via upload
    Crear números aleatorios

---
+++ Numeros_Aleatorios.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Aleatorio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Números aleatorios!");
+            string salir;
+            int numero;
+            int aleatorio;
+            do
+            {
+                Console.WriteLine("Cuantos numero aleatorios desea: ");
+                numero= Convert.ToInt32( Console.ReadLine());
+
+                for (int i=0;i<numero; i++)
+                {
+
+                   
+                    var random = new Random();
+                    aleatorio = random.Next(6);
+
+                    Console.Write(aleatorio);
+                    
+                }
+                Console.WriteLine("");
+
+                Console.WriteLine("Desea salir ? y/n");
+                salir = Console.ReadLine();
+            } while (salir != "y");
+
+        }
+    }
+}
